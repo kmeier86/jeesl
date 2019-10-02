7acd9ba940f6cb4e9c0a7253f3785774a3204801
Thorsten-K <t.kisner@web.de>
2019-10-02 20:47:26 +0200

    SbSearchBean added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/bean/sb/SbSearchBean.java
@@ -0,0 +1,6 @@
+package org.jeesl.interfaces.bean.sb;
+
+public interface SbSearchBean extends SbSingleBean
+{
+	void triggerSbSearch();
+}
--- prototype/src/main/java/org/jeesl/controller/handler/sb/SbSearchHandler.java
+++ prototype/src/main/java/org/jeesl/controller/handler/sb/SbSearchHandler.java
@@ -1,13 +1,11 @@
 package org.jeesl.controller.handler.sb;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import org.jeesl.interfaces.bean.sb.SbSingleBean;
+import org.jeesl.interfaces.bean.sb.SbSearchBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.sf.ahtutils.interfaces.facade.SearchUtilsFacade;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
 public class SbSearchHandler <T extends EjbWithId> extends SbSingleHandler<T>
@@ -15,52 +13,35 @@ public class SbSearchHandler <T extends EjbWithId> extends SbSingleHandler<T>
 	final static Logger logger = LoggerFactory.getLogger(SbMultiHandler.class);
 	private static final long serialVersionUID = 1L;
 
-	private SearchUtilsFacade fUtils;
-	private String searchText;
+	private final SbSearchBean bean;
 
+	private String searchText; public String getSearchText() {return this.searchText;} public void setSearchText(String searchText) {this.searchText = searchText;}
 
-
-	public SbSearchHandler(Class<T> c, SbSingleBean bean, SearchUtilsFacade fUtils) {
-		super(c, bean);
+	public SbSearchHandler(Class<T> c, SbSearchBean bean)
+	{
+		super(c,bean);
+		this.bean=bean;
 		logger.trace("creating handler: " + SbSearchHandler.class.getSimpleName());
-		this.fUtils =  fUtils;
 		this.searchText ="";
 	}
 
-	public String getSearchText() {
-		return this.searchText;
-	}
-
-	public void setSearchText(String searchText) {
-		this.searchText = searchText;
-	}
-
-	public void handleKeyEvent() {
+	public void handleKeyEvent()
+	{
 		logger.trace("handle search event: " + SbSearchHandler.class.getSimpleName());
 		this.clear();
-
-		List<T> result = new ArrayList<T>();
-		try {
-			if(searchText != "") {
-				result = fUtils.callbackSearch(this.c, searchText);
-			}
-			this.update(result);
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(searchText != "")
+		{
+			bean.triggerSbSearch();
 		}
 	}
 
-	public void cancelEvent() {
+	public void cancelEvent()
+	{
 		logger.trace("cancel search event: " + SbSearchHandler.class.getSimpleName());
 		this.clear();
 		this.searchText ="";
 		this.update(new ArrayList<T>());
 	}
 
-	@Override
-	public String toString() {
-		return "handler name: " + this.getClass().getSimpleName() + " for: " +this.c.getSimpleName();
-	}
-
-
+	@Override public String toString() {return "handler name: " + this.getClass().getSimpleName() + " for: " +this.c.getSimpleName();}
 }
