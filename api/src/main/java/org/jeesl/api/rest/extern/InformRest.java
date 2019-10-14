39c7f437233b4b04384830cbc769b24117cad27f
petersen <petersen@aht-group.com>
2019-10-14 19:38:11 +0200

    InformRest expanded

--- api/src/main/java/org/jeesl/api/rest/extern/InformRest.java
+++ api/src/main/java/org/jeesl/api/rest/extern/InformRest.java
@@ -2,20 +2,38 @@ package org.jeesl.api.rest.extern;
 
 import java.util.List;
 
+import javax.ws.rs.Encoded;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/API/InformAPI")
+
+@Encoded @Path("/API/InformAPI")
 public interface InformRest
 {				
-	@GET @Path("/Workflows/WorkflowGroups")
+	String WorkflowGroup = "";
+
+    @GET @Path("/Workflows/WorkflowGroups")
 	@Produces(MediaType.TEXT_PLAIN) String sWorkflows();
 	
 	@GET @Path("/Countries/Index")
 	@Produces(MediaType.TEXT_PLAIN) String sCountries();
 	
+    @GET @Path("/Countries/GetByGroup/Europe")  
+    @Produces(MediaType.TEXT_PLAIN) String sCountriesds();
+	
 	@GET @Path("/Workflows/WorkflowGroups")
 	@Produces(MediaType.APPLICATION_JSON) List<String> workflows();
+	
+    @GET @Path("/Indicators/Index")
+    @Produces("*")  List<Object>  sIndicators();
+    
+    @GET @Path("/Processes/GetByWorkflowId/{id}")
+    @Produces("*")  List<Object>  sGetProcessesByWorkflowId( @PathParam("id") String id);
+    
+    @GET @Path("/Countries/Scores/?WorkflowId={WorkflowId}&Iso3={country}")
+    @Produces("*")  List<Object>  sScoresByWfId(  @PathParam("WorkflowId") String id, @PathParam("country") String Country);
+    
 }
