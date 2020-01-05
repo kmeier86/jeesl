fba94b2a926f7f1f837b99721b44210e5e9fd419
Thorsten-K <t.kisner@web.de>
2020-01-05 09:35:19 +0100

    generics of TsInterval ordered

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsCategory.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsCategory.java
@@ -3,21 +3,21 @@ package org.jeesl.interfaces.model.module.ts.config;
 import java.io.Serializable;
 
 import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
 import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
 
 import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
-import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
 
-public interface JeeslTsInterval <L extends UtilsLang, D extends UtilsDescription,
-							S extends UtilsStatus<S,L,D>,
-							G extends JeeslGraphic<L,D,?,?,?>>
+public interface JeeslTsCategory <L extends UtilsLang, D extends UtilsDescription,
+									S extends UtilsStatus<S,L,D>,
+									G extends JeeslGraphic<L,D,?,?,?>>
 					extends Serializable,EjbPersistable,
-								EjbWithCode,UtilsStatusFixedCode,
-//								JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
+								EjbWithCode,
+								EjbWithCodeGraphic<G>,
 								UtilsStatus<S,L,D>
 {	
-	public enum Code{irregular,minute,minute10,day}
+
 }
