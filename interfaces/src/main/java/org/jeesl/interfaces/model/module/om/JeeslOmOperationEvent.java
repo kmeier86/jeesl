9a206653e993872d89a956d3ffa2f5af98a0762b
Thorsten-K <t.kisner@web.de>
2020-01-28 05:28:00 +0100

    OmOpEvent added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/om/JeeslOmOperationEvent.java
@@ -0,0 +1,26 @@
+package org.jeesl.interfaces.model.module.om;
+
+import java.io.Serializable;
+
+import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
+import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
+import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
+
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
+
+public interface JeeslOmOperationEvent <L extends UtilsLang, D extends UtilsDescription,
+									S extends UtilsStatus<S,L,D>,
+									G extends JeeslGraphic<L,D,?,?,?>>
+					extends Serializable,EjbPersistable,
+								EjbWithCode,UtilsStatusFixedCode,
+								JeeslOptionRestDownload,
+								EjbWithCodeGraphic<G>,
+								UtilsStatus<S,L,D>
+{	
+	public enum Code{purchase,deployment}
+}
