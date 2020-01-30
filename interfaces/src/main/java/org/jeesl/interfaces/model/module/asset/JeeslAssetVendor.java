dd02bc65a899700fd71e97dd3c5e9bdfebc6b1c3
Thorsten-K <t.kisner@web.de>
2020-01-30 21:05:13 +0100

    Vendor added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/asset/JeeslAssetVendor.java
@@ -0,0 +1,20 @@
+package org.jeesl.interfaces.model.module.asset;
+
+import java.io.Serializable;
+
+import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
+import net.sf.ahtutils.interfaces.model.with.code.EjbWithNonUniqueCode;
+import net.sf.ahtutils.model.interfaces.with.EjbWithName;
+
+public interface JeeslAssetVendor <REALM extends JeeslAssetRealm<?,?,REALM,?>>
+		extends Serializable,EjbSaveable,EjbWithName,EjbWithNonUniqueCode
+{
+	REALM getRealm();
+	void setRealm(REALM realm);
+	
+	long getRealmIdentifier();
+	void setRealmIdentifier(long realmIdentifier);
+	
+	String getUrl();
+	void setUrl(String url);
+}
