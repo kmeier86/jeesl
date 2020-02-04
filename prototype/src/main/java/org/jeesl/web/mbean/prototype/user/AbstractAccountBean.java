0125c6bae137ac714b6f1b318fa7c3921c89c91b
Thorsten-K <t.kisner@web.de>
2020-02-04 13:57:04 +0100

    interfaces restructured

--- prototype/src/main/java/org/jeesl/web/mbean/prototype/user/AbstractAccountBean.java
+++ prototype/src/main/java/org/jeesl/web/mbean/prototype/user/AbstractAccountBean.java
@@ -11,6 +11,8 @@ public class AbstractAccountBean <USER extends JeeslUser<?>> implements Serializ
 	final static Logger logger = LoggerFactory.getLogger(AbstractAccountBean.class);
 	private static final long serialVersionUID = 1L;
 	
+	protected USER user; public USER getUser() {return user;}
+	
 	protected String pwd0; public String getPwd0() {return pwd0;} public void setPwd0(String pwd0) {this.pwd0 = pwd0;}
 	protected String pwd1; public String getPwd1() {return pwd1;} public void setPwd1(String pwd1) {this.pwd1 = pwd1;}
 	protected String pwd2; public String getPwd2() {return pwd2;} public void setPwd2(String pwd2){this.pwd2 = pwd2;}
@@ -18,5 +20,17 @@ public class AbstractAccountBean <USER extends JeeslUser<?>> implements Serializ
 	public AbstractAccountBean()
 	{
 
+	}
+	
+	protected boolean validEntries()
+	{
+		if(pwd0!=null && pwd1!=null && pwd2!=null)
+		{
+			pwd0 = pwd0.trim();
+			pwd1 = pwd1.trim();
+			pwd2 = pwd2.trim();
+			return true;
+		}
+		else {return false;}
 	}
 }
